3936e2e1da40618dfb9e5dcac1f6859a95749064
warishasan <waris.hasan00@gmail.com>
2020-07-19 13:51:18 +1200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  let [counter,updateCounter] = React.useState(0)
+  let [isWinter, setWeather] = React.useState(true)
+  
+  return (
+    <div className= {`box ${isWinter ? "winter" : "summer"}`}>
+
+      <h1> It is {isWinter === true? "Winter":"summer"} </h1>
+      <br/>
+      
+      <h1> The temperature is {counter}  </h1>
+
+    <button onClick = {()=> updateCounter(++counter) } > + </button>
+    
+    <button onClick = {()=> updateCounter(--counter) } > - </button>
+      
+    <button onClick = {()=> setWeather(!isWinter) } > Change Weather </button>
+
+    </div>
+  );
+}
+
+export default App;
